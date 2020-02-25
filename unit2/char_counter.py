efd07cf20b1259ddc04b0448f1a00f0bee2e56db
LuBowen <359352176@qq.com>
2020-02-25 10:37:55 +0800

    char_counter

--- unit2/char_counter.py
+++ unit2/char_counter.py
@@ -1,7 +1,8 @@
 import os
+import numpy as np
 path = "C:/Users/Rainey/Desktop/git/unit2/readme.md"
 if os.path.isfile(path):
     f = open(path, 'r')
-    print(len(f.read()))
+    data = f.read()
+    print(len(data.split()))
     f.close()
-
